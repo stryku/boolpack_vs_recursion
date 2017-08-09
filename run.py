5620b08c6edf4b87852ab0ace32c7b2a5f27e4d3
Mateusz Janek <stryku@users.noreply.github.com>
2017-08-09 13:33:23 +0200

    Update run.py

--- run.py
+++ run.py
@@ -28,10 +28,6 @@ def run_test(compiler, proposal_version, case, disjunction_namespace, test_type_
 	return end - start
 
 
-def print_params(compiler, proposal_version, case, test_type_namespace, disjunction_namespace, result):
-	print(compiler + ', \t' + proposal_version + ', \t' + case + ', \t' + test_type_namespace + ', \t' + disjunction_namespace + ', \t' + result)
-
-
 def run_tests(times, compiler, proposal_version, case, disjunction_namespace, test_type_namespace):
 	sys.stdout.flush()
 	sum = 0.0
@@ -43,48 +39,39 @@ def run_tests(times, compiler, proposal_version, case, disjunction_namespace, te
 	print(compiler + ', \t' + proposal_version + ', \t' + case + ', \t' + test_type_namespace + ', \t' + disjunction_namespace + ', \t' + str(sum / times))
 
 
-TIMES_TO_RUN=10
+TIMES_TO_RUN=500
 CLANG='clang++-4.0'
 GXX='g++-6'
 
 run_tests(TIMES_TO_RUN, CLANG, 'fold', 'best_case',    'prop', 'light')
+run_tests(TIMES_TO_RUN, CLANG, 'bools', 'best_case',    'prop', 'light')
 run_tests(TIMES_TO_RUN, CLANG, 'fold', 'best_case',    'std',  'light')
+
 run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'prop', 'light')
+run_tests(TIMES_TO_RUN, CLANG, 'bools', 'worse_case', 'prop', 'light')
 run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'std',  'light')
 
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'best_case',    'prop', 'light')
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'best_case',    'std',  'light')
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'worse_case', 'prop', 'light')
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'worse_case', 'std',  'light')
-
 run_tests(TIMES_TO_RUN, CLANG, 'fold', 'best_case',    'prop', 'heavy')
-run_tests(TIMES_TO_RUN, CLANG, 'fold', 'best_case',    'std',  'heavy')
-run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'prop', 'heavy')
-run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'std',  'heavy')
-
 run_tests(TIMES_TO_RUN, CLANG, 'bools', 'best_case',    'prop', 'heavy')
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'best_case',    'std',  'heavy')
+run_tests(TIMES_TO_RUN, CLANG, 'fold', 'best_case',    'std',  'heavy')
+
+run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'prop', 'heavy')
 run_tests(TIMES_TO_RUN, CLANG, 'bools', 'worse_case', 'prop', 'heavy')
-run_tests(TIMES_TO_RUN, CLANG, 'bools', 'worse_case', 'std',  'heavy')
+run_tests(TIMES_TO_RUN, CLANG, 'fold', 'worse_case', 'std',  'heavy')
 
 
 run_tests(TIMES_TO_RUN, GXX, 'fold', 'best_case',    'prop', 'light')
+run_tests(TIMES_TO_RUN, GXX, 'bools', 'best_case',    'prop', 'light')
 run_tests(TIMES_TO_RUN, GXX, 'fold', 'best_case',    'std',  'light')
+
 run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'prop', 'light')
+run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'prop', 'light')
 run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'std',  'light')
 
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'best_case',    'prop', 'light')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'best_case',    'std',  'light')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'prop', 'light')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'std',  'light')
-
 run_tests(TIMES_TO_RUN, GXX, 'fold', 'best_case',    'prop', 'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'fold', 'best_case',    'std',  'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'prop', 'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'std',  'heavy')
-
 run_tests(TIMES_TO_RUN, GXX, 'bools', 'best_case',    'prop', 'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'best_case',    'std',  'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'prop', 'heavy')
-run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'std',  'heavy')
+run_tests(TIMES_TO_RUN, GXX, 'fold', 'best_case',    'std',  'heavy')
 
+run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'prop', 'heavy')
+run_tests(TIMES_TO_RUN, GXX, 'bools', 'worse_case', 'prop', 'heavy')
+run_tests(TIMES_TO_RUN, GXX, 'fold', 'worse_case', 'std',  'heavy')
